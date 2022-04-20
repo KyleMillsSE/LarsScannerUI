ab4f5ef37fd36087225f0b6b56ee1eb089a18eab
Kyle Mills <kyle.mills@tungsten-network.com>
2022-04-20 12:47:08 +0100

    Login component extended to capture login events

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Login extends Component {
+class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = {
+            username: '',
+            password: '',
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        const { username, password } = this.state;
+        alert(`Details submitted: ${username} ${password}`);
+        event.preventDefault();
     }
 
     render() {
+        const { username, password } = this.state;
         return (
-          <div className="login_box">
-            <p>Login</p>
+          <div className="login">
+            <h3>Login</h3>
+            <form onSubmit={this.handleSubmit}>
+              <div>
+                <label htmlFor="username">
+                  Username:
+                  <input type="text" value={username} name="username" />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="password">
+                  Password:
+                  <input type="password" value={password} name="password" />
+                </label>
+              </div>
+              <input type="submit" value="submit" />
+            </form>
           </div>
         );
     }
